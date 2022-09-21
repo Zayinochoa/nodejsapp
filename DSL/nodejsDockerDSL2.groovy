f3050fbdb1fb1668e2f871cae6d696e85c5e0747
Zayinochoa <107062963+Zayinochoa@users.noreply.github.com>
2022-09-20 21:53:04 -0400

    Create nodejsDockerDSL2.groovy

---
+++ DSL/nodejsDockerDSL2.groovy
@@ -0,0 +1,44 @@
+job('Aplicacion Node.js Docker DSL2') {
+    description('Aplicación Node JS Docker DSL para el curso de Jenkins')
+    scm {
+        git('https://github.com/Zayinochoa/simple-java-maven-app.git', 'master') { node ->
+            node / gitConfigName('Zayinochoa')
+            node / gitConfigEmail('bensaj2212@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/7 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs')
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('zayinochoa/nodejsapp')
+            tag('${GIT_REVISION,length=7}')
+            registryCredentials('docker-hub')
+            forcePull(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+    publishers {
+	slackNotifier {
+            notifyAborted(true)
+            notifyEveryFailure(true)
+            notifyNotBuilt(false)
+            notifyUnstable(false)
+            notifyBackToNormal(true)
+            notifySuccess(true)
+            notifyRepeatedFailure(false)
+            startNotification(false)
+            includeTestSummary(false)
+            includeCustomMessage(false)
+            customMessage(null)
+            sendAs(null)
+            commitInfoChoice('NONE')
+            teamDomain(null)
+            authToken(null)
+        }
+    }
+}
